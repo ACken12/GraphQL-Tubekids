3503302366e85a3dfec539d69e9580301f067841
ACken <kendallangulo2gmail.com>
2024-04-22 22:41:38 -0600

    Se agrega los resolvers del schema

---
+++ resolvers.js
@@ -0,0 +1,57 @@
+// You need the tables of the database
+const Playlist = require('../server/models/PlaylistModel'); 
+const Kids = require('../server/models/kidsModel');
+const Video = require('../server/models/VideoModel');
+const User = require('../server/models/UserModel');
+const mongoose = require('../node_modules/mongoose');
+const userId = new mongoose.Types.ObjectId("66122539b8f20a6b79c266a2");
+
+const resolvers = {
+    Query: {
+       
+        hello: () => {
+            return "Hola Mundo";
+        },
+        playlists: async (_, {id}) => {
+
+            const playlist = await Playlist.find({ ownerId: id })
+                .populate('ownerId')
+                .populate('profilesPartners', "path"["profilesPartners", 0, "profilesPartners"], null, { strictPopulate: false })
+                .populate('videos', "path"["videos", 0, "videos"], null, { strictPopulate: false }).exec();
+
+            console.log(playlist);
+            return playlist;
+        },
+        Profiles: async (_, {id}) => {
+            const kids = await Kids.find({ idUser: id })
+                .populate('idUser').exec();
+
+            return kids;
+        },
+        playlist: async (_, { id }) => {
+            const playlis = await Playlist.find({ _id: id })
+                .populate('ownerId')
+                .populate('profilesPartners', "path"["profilesPartners", 0, "profilesPartners"], null, { strictPopulate: false })
+                .populate('videos', "path"["videos", 0, "videos"], null, { strictPopulate: false }).exec();
+            return playlis;
+        },
+        videos: async (_,{id}) => {
+            const video = await Video.find({_id:id}).populate('userId').exec();
+            return video;
+        },
+        playlistkids: async (_,{id}) => {
+
+            const playlis = await Playlist.find({profilesPartners:id})
+            .populate('ownerId')
+            .populate('profilesPartners', "path"["profilesPartners", 0, "profilesPartners"], null, { strictPopulate: false })
+            .populate('videos', "path"["videos", 0, "videos"], null, { strictPopulate: false }).exec();
+            return playlis;
+
+        }
+    }
+}
+
+module.exports = {
+    resolvers
+};
+
